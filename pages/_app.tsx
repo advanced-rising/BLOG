470c89697f37325c0895df135d6def7ff43bc805
risingcore <vcjsm2283@gmail.com>
2022-07-27 22:53:35 +0900

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,8 +30,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <ScrollObserver>
         <ProgressBar />
         <ScrollProgressBar />
+        <LogRocket />
         <LayoutWrapper>
-          <LogRocket />
           <Component {...pageProps} />
         </LayoutWrapper>
       </ScrollObserver>

c647205a95c2d6ddb892abeaa09aa3105562d12f
risingcore <vcjsm2283@gmail.com>
2022-08-03 20:54:19 +0900

    update

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -58,30 +58,35 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
         <ul>
           {!filteredBlogPosts.length && 'No posts found.'}
           {displayPosts.map((frontMatter) => {
-            const { slug, date, title: frontMatterTitle, summary, tags } = frontMatter;
+            const { slug, date, title: frontMatterTitle, summary, tags, comment, code } = frontMatter;
             return (
               <li key={slug} className='py-4'>
                 <article className='space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0'>
                   <dl>
                     <dt className='sr-only'>Published on</dt>
-                    <dd className='text-base font-medium leading-6 text-gray-500 dark:text-gray-400'>
+                    <dd className='text-xs font-medium leading-6 text-gray-500 dark:text-gray-400'>
                       <time dateTime={date}>{formatDate(date)}</time>
                     </dd>
                   </dl>
                   <div className='space-y-3 xl:col-span-3'>
                     <div>
                       <h3 className='text-2xl font-bold leading-8 tracking-tight'>
-                        <Link href={`/${menu}/${slug}`} className='text-gray-900 dark:text-gray-100'>
+                        <Link href={`/devops/${slug}`} className='text-gray-900 dark:text-gray-100'>
                           {frontMatterTitle}
                         </Link>
                       </h3>
+
+                      <div>
+                        <div className=' max-w-none text-gray-500 dark:text-gray-400'>{summary}</div>
+                        <div className=' max-w-none text-gray-500 dark:text-gray-400'>{comment}</div>
+                        <div className=' max-w-none text-gray-500 dark:text-gray-400'>{code}</div>
+                      </div>
                       <div className='flex flex-wrap'>
                         {tags.map((tag) => (
                           <Tag key={tag} text={tag} />
                         ))}
                       </div>
                     </div>
-                    <div className='prose max-w-none text-gray-500 dark:text-gray-400'>{summary}</div>
                   </div>
                 </article>
               </li>
@@ -89,7 +94,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
           })}
         </ul>
       </div>
-      {pagination && pagination.totalPages > 1 && !searchValue && (
+      {menu && pagination && pagination.totalPages > 1 && !searchValue && (
         <Pagination currentPage={pagination.currentPage} totalPages={pagination.totalPages} menu={menu} />
       )}
     </>
